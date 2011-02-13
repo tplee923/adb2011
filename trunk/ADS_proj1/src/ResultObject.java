15f24447aff0fc9390a0e17fb1c63c54fc630696
feirenlei@gmail.com <feirenlei@gmail.com@4fcfe7fd-d0dc-7682-41b7-de80cdc32d70>
2011-02-13 03:43:47 +0000

    init commit

---
+++ trunk/ADS_proj1/src/ResultObject.java
@@ -0,0 +1,36 @@
+
+public class ResultObject {
+	
+	public ResultObject(String title, String url, String summary){
+		this.title = title;
+		this.url = url;
+		this.summary = summary;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getSummary() {
+		return summary;
+	}
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+	
+	public String toString(){
+		return "Title:"+title + "\n" +"URL:" + url+ "\n" + "Summary:" + summary +"\n"; 
+	}
+	
+	private String title;
+	private String url;
+	private String summary;
+}
